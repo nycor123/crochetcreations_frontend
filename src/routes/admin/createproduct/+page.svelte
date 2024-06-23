b4fc5d289f80f9c6c686eacfeb585d946d862615
Nycor Estrada <nycor@192.168.68.111>
2024-06-24 01:06:17 +0800

    Allow sorting of images during new product creation

--- src/routes/admin/createproduct/+page.svelte
+++ src/routes/admin/createproduct/+page.svelte
@@ -4,10 +4,10 @@
         description: "",
         price: "",
         listedForSale: "",
-        imageIds: []
+        images: []
     };
 
-    let _files = [];
+    let _images = [];
     let _isSaving = false;
 
     async function saveProduct(event) {
@@ -20,9 +20,19 @@
         }
         try {
             let products = [];
-            if (_files != null && _files.length > 0) {
-                _product.imageIds = await uploadImages(_files);
+            // upload images
+            if (_images != null && _images.length > 0) {
+                let productImages = [];
+                for (let image of _images) {
+                    let productImage = {
+                        id: await uploadImage(image.file),
+                        priority: image.priority
+                    };
+                    productImages.push(productImage);
+                }
+                _product.images = productImages;
             }
+            // save product
             products.push(_product);
             let response = await fetch("http://localhost:8080/api/v1/products", {
                 method: "POST",
@@ -37,9 +47,11 @@
             if (!response.ok) {
                 throw new Error(resJson.message);
             }
+            // cleanup
             _isSaving = false;
             form.classList.remove("was-validated");
             triggerAlert("success", "Successfully saved product - "); //TODO: create product link
+            _images = [];
             form.reset();
         } catch(err) {
             _isSaving = false;
@@ -47,23 +59,19 @@
         }
     }
 
-    async function uploadImages(files) {
-        let imageIds = [];
-        for (const file of files) {
-            let formData = new FormData();
-            formData.append("file", file);
-            let response = await fetch("http://localhost:8080/api/v1/images/upload", {
-                method: "POST",
-                credentials: "include",
-                headers: {
-                    "Accept": "application/json"
-                },
-                body: formData
-            });
-            let resJson = await response.json();
-            imageIds.push(resJson.id);
-        }   
-        return imageIds;
+    async function uploadImage(file) {
+        let formData = new FormData();
+        formData.append("file", file);
+        let response = await fetch("http://localhost:8080/api/v1/images/upload", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Accept": "application/json"
+            },
+            body: formData
+        });
+        let resJson = await response.json();
+        return resJson.id;
     }
 
     function triggerAlert(type, message, productLink = "#", errorMsg) {
@@ -89,6 +97,55 @@
         alertPlaceholder.append(wrapper);
     }
 
+    async function addNewImage(event) {
+        let image = {
+            file: event.target.files[0],
+            url: await generateUrl(event.target.files[0]),
+            priority: _images.length + 1
+        };
+        _images = [..._images, image];
+    }
+    
+    async function generateUrl(file) {
+        return new Promise((resolve) => {
+            let reader = new FileReader();
+            reader.onload = (e) => {
+                resolve(e.target.result);
+            };
+            reader.readAsDataURL(file);
+        });
+    }
+
+    function allowDrop(event) {
+        event.preventDefault();
+    }
+
+    function drop(event) {
+        event.preventDefault();
+        let data = JSON.parse(event.dataTransfer.getData("text"));
+        let destinationDiv = event.target.parentElement;
+        let img1 = document.getElementById(data.imgId);
+        let img2 = event.target;
+        for (let image of _images) {
+            if (image.url === img1.src) {
+                image.priority = Number(destinationDiv.id.replace('img-', ''));
+            }
+            if (image.url === img2.src) {
+                image.priority = Number(data.divId.replace('img-', ''));
+            }
+        }
+        _images.sort((img1, img2) => img1.priority - img2.priority);
+        _images = _images;
+    }
+
+    function drag(event) {
+        let data = {
+            divId: event.target.parentElement.id,
+            imgId: event.target.id
+        };
+        event.dataTransfer.setData("text/plain", JSON.stringify(data));
+    }
+
     function cancel(event) {
         window.location.href = "/admin";
     }
@@ -96,7 +153,7 @@
 </script>
 
 <div class="row mx-3 my-3">
-    <div class="col-sm-12 col-md-6 col-lg-4 mx-auto">
+    <div class="col-12 col-md-8 col-lg-6 mx-auto">
         <div id="alertPlaceholder"></div>
 
         <form on:submit={saveProduct} novalidate>
@@ -122,26 +179,51 @@
                     <label for="priceInput" class="form-label">Price</label>
                 </div>
             </div>
-            
-            <div class="mb-3">
-                <label for="imagesInput" class="form-label">Pictures</label>
-                <input 
-                    id="imagesInput" 
-                    type="file" 
-                    accept="image/png, image/jpeg" 
-                    class="form-control" 
-                    multiple 
-                    bind:files={ _files } >
+
+            <div class="row mb-3">
+                <p class="form-label">Images</p>
+                {#if _images.length > 0}
+                    {#each _images as image}
+                        <div 
+                            id={"img-" + image.priority} 
+                            class="col-4 col-xl-3 my-2" 
+                            on:dragover={allowDrop} 
+                            on:drop={drop}
+                            role="none">
+                            {#await generateUrl(image.file)}
+                                <span>...</span>
+                            {:then generatedUrl}
+                                <img 
+                                    id={image.file.name}
+                                    src={generatedUrl}
+                                    alt="{image.file.name}" 
+                                    width="100%" 
+                                    draggable="true"
+                                    on:dragstart={drag} />
+                            {/await}
+                        </div>  
+                    {/each}
+                {/if}
+                <div class="col-4 col-xl-3 my-2">   
+                    <label for="add-image">
+                        <div style="width: 100%; background-color: lightgray; padding: 1.5em;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="gray" class="bi bi-plus-lg" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
+                            </svg>
+                        </div>
+                    </label>
+                    <input id="add-image" class="position-absolute" type="file" accept="image/png, image/jpeg" style="visibility: hidden;" on:change={addNewImage}>
+                </div>
             </div>
 
             <div class="form-check mb-3">
-                <input id="listForSaleInput" type="checkbox" class="form-check-input" bind:checked={ _product.listedForSale }>
+                <input id="listForSaleInput" type="checkbox" class="form-check-input" bind:checked={_product.listedForSale}>
                 <label for="listForSaleInput" class="form-check-label">List for sale?</label>
             </div>
 
             <div class="mt-5">
                 <button type="submit" class="btn btn-outline-success me-2">Save</button>
-                <button type="button" class="btn btn-outline-danger" on:click={ cancel }>Cancel</button>
+                <button type="button" class="btn btn-outline-danger" on:click={cancel}>Cancel</button>
             </div>
         </form>
 
@@ -163,5 +245,12 @@
 
     div button {
         width: 5em;
+    }
+
+    input::file-selector-button {
+        width: 100px;
+        height: 100px;
+        background: url(https://placehold.co/100);
+        border: none;
     }
 </style>
